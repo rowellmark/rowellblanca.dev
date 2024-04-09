aa5fb33eca5f7fe373b5f23fcbb5cbac062c1bf1
rowell.blanca <rowell.blanca@august99.com>
2024-04-09 16:23:47 +0800

    Initial Commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,20 @@
+import { FloatingNav } from "@/components/ui/floating-navbar";
 import Image from "next/image";
 
+
+
 export default function Home() {
+
+  const nav = [
+    { name: 'Home', link: '/' },
+    { name: 'About', link: '/about' }
+  ];
+  
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <>
+    
+    <FloatingNav navItems={nav } />
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 h-[2000px]">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
@@ -108,6 +120,7 @@ export default function Home() {
           </p>
         </a>
       </div>
-    </main>
+      </main>
+      </>
   );
 }
