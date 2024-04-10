f5f81c28fe930b46f25e4843cb7ba89cc8b8a157
rowell.blanca <rowell.blanca@august99.com>
2024-04-10 22:22:47 +0800

    update

--- components/ui/contactForm.tsx
+++ components/ui/contactForm.tsx
@@ -4,6 +4,7 @@ import React from "react";
 export default async function Contact() {
     async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
         event.preventDefault();
+        const formData = new FormData(event.currentTarget);
         const response = await fetch("https://api.web3forms.com/submit", {
             method: "POST",
             headers: {
@@ -12,9 +13,9 @@ export default async function Contact() {
             },
             body: JSON.stringify({
                 access_key: "b9fe178c-c566-40f8-bec2-39bb41fb27a7",
-                name: event.target.name.value,
-                email: event.target.email.value,
-                message: event.target.message.value,
+                name: formData.get("name"),
+                email: formData.get("email"),
+                message: formData.get("message"),
             }),
         });
         const result = await response.json();
