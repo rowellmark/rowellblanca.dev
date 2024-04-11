8758ce32119cd5ea29e861a17baf50de4ed419a0
rowell.blanca <rowell.blanca@august99.com>
2024-04-11 19:24:02 +0800

    update

--- components/ui/primaryButton.tsx
+++ components/ui/primaryButton.tsx
@@ -1,6 +1,22 @@
 import Link from "next/link";
 
-export default function PrimaryButton({ label, link, className }) {
+
+interface PrimaryButtonProps {
+    label: string;
+    link?: string;
+    className?: string;
+}
+export default function PrimaryButton({ label, link, className }: PrimaryButtonProps) {
+
+    // Check if link is provided
+    if (!link) {
+        return (
+            <button className={className}>
+                {label}
+            </button>
+        );
+    }
+
     return (
         <>
             <Link href={link} className={`${className} inline-block px-12 py-4 rounded-full bg-accent-color font-bold text-white tracking-widest uppercase transform hover:scale-105 hover:bg-accent-color-slate transition-colors duration-200`}>
