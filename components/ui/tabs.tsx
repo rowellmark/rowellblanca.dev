88a78173e99b585722e983e622f0528f628e7eca
rowell.blanca <rowell.blanca@august99.com>
2024-04-17 23:01:53 +0800

    fix lint issue

--- components/ui/tabs.tsx
+++ components/ui/tabs.tsx
@@ -16,31 +16,33 @@ export function Tab({ tabs }: TabProps) {
 
     return (
         <div>
-            <div className="flex space-x-4 tabButton py-5 justify-center">
+            <div className="flex space-x-4">
                 {tabs.map((tab, index) => (
                     <button
                         key={index}
                         className={`${index === activeTab
-                            ? 'border-b-2 border-accent-color text-accent-color'
+                            ? 'border-b-2 border-blue-500 text-blue-500'
                             : 'text-base text-white'
-                            } focus:outline-none uppercase text-base font-normal px-5`}
+                            } focus:outline-none`}
                         onClick={() => setActiveTab(index)}
                     >
                         {tab.label}
                     </button>
                 ))}
             </div>
-            <div className="tabContainer flex pt-7">
-                {tabs[activeTab].content.map(item => (
-                    
-                    <div className="tabCol w-1/3 p-4" key={item.key}>
-                        <a href="#">
-                            <canvas width="300" height="200" className="bg-slate-400 block w-full h-auto"></canvas>
-                            {item.text}
-                        </a>
-                       
+            <div>
+                {tabs[activeTab].content.map((item, index) => (
+                    // Type assertion to let TypeScript know that item is not null or undefined
+                    <div className="tabCol w-1/3 p-4" key={index}>
+                        {item && (
+                            <>
+                                <a href="#">
+                                    <canvas width="300" height="200" className="bg-slate-400 block w-full h-auto"></canvas>
+                                    {item.text}
+                                </a>
+                            </>
+                        )}
                     </div>
-                   
                 ))}
             </div>
         </div>
