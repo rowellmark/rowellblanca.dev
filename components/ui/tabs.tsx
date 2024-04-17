02f1e5898cee0cd4abb3d0f0ce578df1a8cb5522
rowell.blanca <rowell.blanca@august99.com>
2024-04-17 22:53:40 +0800

    additional tabs

--- components/ui/tabs.tsx
+++ components/ui/tabs.tsx
@@ -16,25 +16,29 @@ export function Tab({ tabs }: TabProps) {
 
     return (
         <div>
-            <div className="flex space-x-4">
+            <div className="flex space-x-4 tabButton py-5 justify-center">
                 {tabs.map((tab, index) => (
                     <button
                         key={index}
                         className={`${index === activeTab
-                            ? 'border-b-2 border-blue-500 text-blue-500'
+                            ? 'border-b-2 border-accent-color text-accent-color'
                             : 'text-base text-white'
-                            } focus:outline-none`}
+                            } focus:outline-none uppercase text-base font-normal px-5`}
                         onClick={() => setActiveTab(index)}
                     >
                         {tab.label}
                     </button>
                 ))}
             </div>
-            <div className="tabContainer">
+            <div className="tabContainer flex pt-7">
                 {tabs[activeTab].content.map(item => (
                     
-                    <div className="tabCol" key={item.key}>
-                        {item.text}
+                    <div className="tabCol w-1/3 p-4" key={item.key}>
+                        <a href="#">
+                            <canvas width="300" height="200" className="bg-slate-400 block w-full h-auto"></canvas>
+                            {item.text}
+                        </a>
+                       
                     </div>
                    
                 ))}
