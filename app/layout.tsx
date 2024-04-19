9d9e45595de6c6dfdd7b1361d630699556227a94
rowell.blanca <rowell.blanca@august99.com>
2024-04-19 12:43:14 +0800

    fix meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,39 @@
 import type { Metadata } from "next";
+import Head from "next/head"; // Import Head component from next/head
+import { ReactNode } from "react"; // Import ReactNode type
 import "./globals.css";
 import Footer from "@/components/footer/footer";
 import Header from "@/components/header/header";
 
-export const metadata: Metadata = {
+export const metadata: Metadata & { image: string } = {
   title: "Rowell Mark Blanca - I build things for the web.",
   description: "I specialize in crafting innovative digital experiences on the web, leveraging cutting-edge technologies and creative design to build engaging and user-friendly websites and applications.",
+  image: "/mypicture.jpg", // Specify the path to your image
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+interface RootLayoutProps {
+  children: ReactNode; // Specify children prop as ReactNode type
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
+      <Head>
+        {/* Set the meta tags for title, description, and image */}
+        <title>{metadata.title as string}</title>
+        <meta name="description" content={metadata.description as string} />
+        <meta property="og:title" content={metadata.title as string} />
+        <meta property="og:description" content={metadata.description as string} />
+        <meta property="og:image" content={metadata.image as string} />
+        <meta name="twitter:title" content={metadata.title as string} />
+        <meta name="twitter:description" content={metadata.description as string} />
+        <meta name="twitter:image" content={metadata.image as string} />
+      </Head>
       <body>
-        <Header /> 
+        <Header />
         {children}
         <Footer />
-        </body>
+      </body>
     </html>
   );
 }
