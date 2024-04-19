d2774dcf6c36fb90d1dae359fb3f563debf24778
rowell.blanca <rowell.blanca@august99.com>
2024-04-19 11:45:15 +0800

    add intro

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
   return (
     <>
       
-      {/* <WelcomeLoading /> */}
+      <WelcomeLoading />
       <Hero />
       <ShowCasePortfolios />
       <ContactSection />
