aec044d8eaa1ed3271b44eed6c9c215dc95b9657
rowell.blanca <rowell.blanca@august99.com>
2024-05-18 21:56:42 +0800

    fix mobile

--- app/mywork/[projects]/page.tsx
+++ app/mywork/[projects]/page.tsx
@@ -119,7 +119,7 @@ export default function Projects({ params }: ProjectProps) {
                             
                                 </div>
 
-                                <div className="projectDetails pt-20 flex justify-between">
+                                <div className="projectDetails pt-20 flex justify-between max-lg:flex-col">
 
                                     <div className="projectDescriptionLeft">
                                         <div className="projectDescription text-lg">
@@ -141,12 +141,12 @@ export default function Projects({ params }: ProjectProps) {
                                             <PrimaryButton link="/about" label="Say Hello!" className=" mt-7" />
                                         </div>
                                     </div>
-                                    <div className="projectDescriptionRight flex">
-                                        <div className="projectClient border-t border-slate-200 px-5">
+                                    <div className="projectDescriptionRight flex max-lg:flex-col">
+                                        <div className="projectClient border-t border-slate-200 px-5 max-lg:py-8 mt-10">
                                             <h3 className="font-bold text-accent-color pb-4 pt-2 text-lg">Client</h3>
                                             <p>Personal Project</p>
                                         </div>
-                                        <div className="projectTechonologies border-t border-slate-200 ml-20 px-5">
+                                        <div className="projectTechonologies border-t border-slate-200 ml-20 px-5 max-lg:ml-0 max-lg:py-8">
                                             <h3 className="font-bold text-accent-color pb-4 pt-2 text-lg">Technologies</h3>
                                             <ul className="list-disc px-4">
                                                 {project[0].technologies.map((technology, index) => (
